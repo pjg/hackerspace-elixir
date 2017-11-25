3c3ebfeb5de64377193d23369009fb66259cd7b4
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-11-25 12:51:05 +0100

    Add test for `moduledoc()`

--- test/hackerspace_test.exs
+++ test/hackerspace_test.exs
@@ -5,4 +5,8 @@ defmodule HackerspaceTest do
   test "greets the world" do
     assert Hackerspace.hello() == :world
   end
+
+  test "moduledoc()" do
+    assert Hackerspace.moduledoc() =~ ~r(Documentation)
+  end
 end
