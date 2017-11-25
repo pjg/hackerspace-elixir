f47aff95ca160def085c950ff243a05e49e72dce
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-11-25 12:47:30 +0100

    Add @doc test

--- lib/hackerspace.ex
+++ lib/hackerspace.ex
@@ -3,8 +3,14 @@ defmodule Hackerspace do
   Documentation for Hackerspace.
   """
 
+  @doc """
+  ## Examples
+    iex> Hackerspace.moduledoc
+    "Documentation for Hackerspace.\\n"
+
+  """
   def moduledoc() do
-    @moduledoc
+   "#{ @moduledoc }"
   end
 
   @doc """
