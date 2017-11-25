1f87b84733cb6bccc66d3ef1cdafb41e93e8f795
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-11-25 11:52:50 +0100

    Initial commit (mix new)

---
+++ lib/hackerspace.ex
@@ -0,0 +1,18 @@
+defmodule Hackerspace do
+  @moduledoc """
+  Documentation for Hackerspace.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hackerspace.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
